e1c7d5e9dbef9e5cab2122cd15bad29697f79133
'femmefatalehaein' <rmfls4359@gmail.com>
2024-05-14 03:55:07 +0900

    simple sort algorithm

--- Algorithm/src/SortingAlogorithm/InsertionSort.java
+++ Algorithm/src/SortingAlogorithm/InsertionSort.java
@@ -1,5 +1,20 @@
 package SortingAlogorithm;
 
 public class InsertionSort {
+		
+	public static void sortiByInsertionSort(int[] arr) {
+		for(int i=1; i<arr.length; i++) {
+			//확인하고자 하는 원소
+			int target = arr[i];
+			int j = i-1;
+			//타겟이 이전 원소보다 크기 전 까지 반복
+			while( j>=0 && target<arr[j] ) {
+				arr[j+1] = arr[j]; //이전 원소를 한 칸씩 뒤로 미룬다.
+				j--;
+			}
+			//while문을 나왔다는것 ! 타겟이 이전 원소보다 작다는 것!
+			arr[j+1] = target;
+		}
+	}
 
 }
--- Algorithm/src/SortingAlogorithm/SelectionSort.java
+++ Algorithm/src/SortingAlogorithm/SelectionSort.java
@@ -2,9 +2,24 @@ package SortingAlogorithm;
 
 public class SelectionSort {
 	
+	public static void swap(int[] arr, int idx1, int idx2){
+		int tmp = arr[idx1];
+		arr[idx1] = arr[idx2];
+		arr[idx2] = tmp;
+	}
+	
 	public static void sortBySelectionSort(int[] arr) {
-		System.out.print("hi");
-		
+	
+		for(int i=0; i<arr.length-1; i++) {
+			int minIdx = i;
+			//최솟값 찾기 for문
+			for(int j=i+1; j<arr.length; j++) {
+				if(arr[j] <arr[minIdx]) {
+					minIdx = j;
+				}
+			}
+			swap(arr, i, minIdx);
+		}
 	}
 
 }
