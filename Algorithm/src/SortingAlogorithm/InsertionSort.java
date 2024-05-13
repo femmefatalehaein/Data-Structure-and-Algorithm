e760ac5b058f8433e93c342f5f44ce98d0dce625
'femmefatalehaein' <rmfls4359@gmail.com>
2024-05-14 03:50:08 +0900

    simple sort algorithm

---
+++ Algorithm/src/SortingAlogorithm/InsertionSort.java
@@ -0,0 +1,5 @@
+package SortingAlogorithm;
+
+public class InsertionSort {
+
+}
--- Algorithm/src/SortingAlogorithm/SelectionSort.java
+++ Algorithm/src/SortingAlogorithm/SelectionSort.java
@@ -3,6 +3,7 @@ package SortingAlogorithm;
 public class SelectionSort {
 	
 	public static void sortBySelectionSort(int[] arr) {
+		System.out.print("hi");
 		
 	}
 
